138dcad47eb15ddea9e146bc3c71441a1a199893
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2026-02-26 07:28:19 +0530

    Implement error handling middleware and add utility functions for API error responses

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,4 +22,13 @@ app.get("/", (req, res) => {
    });
 });
 
+app.use((err, req, res, next) => {
+   const statusCode = err.statusCode || 500;
+
+   res.status(statusCode).json({
+      success: false,
+      message: err.message || "Internal Server Error",
+   });
+});
+
 export default app;
